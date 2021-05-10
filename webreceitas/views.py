d065b53eb2d7f05b7e3b1c5a37632a89edc994b4
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-05-10 09:09:58 -0300

    add files

---
+++ webreceitas/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):    
+    receitas = {
+        1:'Lasanha',
+        2:'Sopa de Legumes',
+        3:'Sorvete',
+        4:'Bolo de Chocolate'
+    }
+    
+    dados = {
+        'nome_das_receitas': receitas
+    }
+    
+    return render(request, 'index.html', dados)
+
+def receita(request):
+    return render(request, 'receita.html')
