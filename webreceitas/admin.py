87a17f107b9f13706ab7cb4cc32622bf7bc4a231
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-05-11 09:35:41 -0300

    admin - add classe para visualizar nome das receitas no admin

--- webreceitas/admin.py
+++ webreceitas/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class ExibirReceita(admin.ModelAdmin):
+    list_display= ('id', 'nome_receita', 'categoria',  'tempo_preparo')
+    list_display_links = ('id', 'nome_receita')
+
+admin.site.register(Receita, ExibirReceita)
