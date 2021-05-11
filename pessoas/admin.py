25b15f8ef56d9ee2153697dd5eefc291b47f471f
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-05-11 10:31:47 -0300

    Exibindo nome das pessoas nas receitas, no models pessosa add funcao

--- pessoas/admin.py
+++ pessoas/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Pessoa
 
 class ExibirPessoa(admin.ModelAdmin):
-    list_display = ('id', 'nome_pessoa', 'email')
+    list_display = ('id', 'nome_pessoa', 'email',)
     search_fields = ('nome_pessoa',)   
     list_display_links = ('id', 'nome_pessoa',)
     list_per_page = 2
--- webreceitas/admin.py
+++ webreceitas/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Receita
 
 class ExibirReceita(admin.ModelAdmin):
-    list_display= ('id', 'nome_receita', 'categoria',  'tempo_preparo')
+    list_display= ('id', 'nome_receita', 'categoria',  'tempo_preparo', 'pessoa')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
