217e64c0085cc9477fb35c7939748c8727ff4d2e
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-05-15 21:34:00 -0300

    views do usuarios: add validações e criado user no banco de dados

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 def cadastro(request):
     if request.method == 'POST':
-        print('cadastro efetuado')
+        nome = request.POST['nome']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        print()
+        
+        if not nome.strip():
+            print('erro nome')
+            return redirect('cadastro')
+        
+        if not email.strip():            
+            return redirect('cadastro')
+        
+        if password != password2:
+            print('erro senha')
+            return redirect('cadastro')
+        
+        if User.objects.filter(email=email).exists():
+            print('user ja cadastrado')
+            return redirect('cadastro')        
+        user =  User.objects.create_user(username=nome, email=email, password=password)
+        user.save
+        
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
